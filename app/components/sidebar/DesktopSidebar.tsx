07ea018f425beb928d5db91384c2063bfa6b3663
RuslanDidun <ruslandidun5@gmail.com>
2023-06-14 20:28:04 -0600

    avatar component

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import { useState } from 'react'
 import useRoutes from "@/app/hooks/useRoutes";
 import DesktopItem from './DesktopItem';
+import { User } from "@prisma/client";
+import Avatar from "../Avatar";
 
+interface DesktopSidebarProps {
+  currentUser: User
+}
 
-const DesktopSidebar = () => {
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({
+  currentUser
+}) => {
 
   const routes = useRoutes()
   const [isOpen, setIsOpen] = useState(false)
@@ -27,8 +34,14 @@ const DesktopSidebar = () => {
           ))}
         </ul>
       </nav>
+      <nav className="mt-4 flex flex-col justify-between items-center">
+        <div onClick={() => setIsOpen(true)}
+          className="cursor-pointer hover:opacity-75 transition" >
+          <Avatar user={currentUser} />
+        </div>
+      </nav>
     </div>
   )
 }
 
-export default DesktopSidebar
+export default DesktopSidebar;
--- app/components/sidebar/MobileItem.tsx
+++ app/components/sidebar/MobileItem.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-
 import clsx from "clsx";
 
 interface MobileItemProps {
@@ -9,12 +8,8 @@ interface MobileItemProps {
   onClick?: () => void;
 }
 
-const MobileItem: React.FC<MobileItemProps> = ({
-  href,
-  icon: Icon,
-  active,
-  onClick
-}) => {
+const MobileItem: React.FC<MobileItemProps> = ({ href, icon: Icon, active, onClick }) => {
+
   const handleClick = () => {
     if (onClick) {
       return onClick();
@@ -22,23 +17,9 @@ const MobileItem: React.FC<MobileItemProps> = ({
   };
 
   return (
-    <Link
-      onClick={handleClick}
-      href={href}
-      className={clsx(`
-        group 
-        flex 
-        gap-x-3 
-        text-sm 
-        leading-6 
-        font-semibold 
-        w-full 
-        justify-center 
-        p-4 
-        text-gray-500 
-        hover:text-black 
-        hover:bg-gray-100
-      `,
+    <Link onClick={handleClick} href={href}
+      className={clsx(`group flex gap-x-3 text-sm leading-6 font-semibold w-full 
+        justify-center p-4 text-gray-500 hover:text-black hover:bg-gray-100`,
         active && 'bg-gray-100 text-black',
       )}>
       <Icon className="h-6 w-6" />
