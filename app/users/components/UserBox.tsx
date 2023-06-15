304a808a915fa6e1e9af89badae0df60d82c9e36
RuslanDidun <ruslandidun5@gmail.com>
2023-06-15 15:26:33 -0600

    getConversations

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import axios from "axios";
+import { useCallback, useState } from "react";
+import { useRouter } from "next/navigation";
+import { User } from "@prisma/client";
+import Avatar from "@/app/components/Avatar";
 
-const UserBox = () => {
-  return (
-    <div>UserBox</div>
-  )
+interface UserBoxProps {
+  data: User
 }
 
-export default UserBox
+const UserBox: React.FC<UserBoxProps> = ({ data }) => {
+
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleClick = useCallback(() => {
+    axios.post('/api/conversations', { userId: data.id })
+      .then((data) => {
+        router.push(`/conversations/${data.data.id}`)
+      }).finally(() => setIsLoading(false));
+  }, [data, router])
+
+  return (
+    <>
+      {/* {isLoading && (
+        <LoadingModal />
+      )} */}
+      <div
+        onClick={handleClick}
+        className="w-full relative flex items-center space-x-3 bg-white p-3 
+          hover:bg-neutral-100 rounded-lg transition cursor-pointer">
+        <Avatar user={data} />
+        <div className="min-w-0 flex-1">
+          <div className="focus:outline-none">
+            <span className="absolute inset-0" aria-hidden="true" />
+            <div className="flex justify-between items-center mb-1">
+              <p className="text-sm font-medium text-gray-900">
+                {data.name}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default UserBox;
